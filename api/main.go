ea549a5dfa75be283e174fea36976a974ff90fa4
marcin2203 <marcin.karpinski.prac@gmail.com>
2024-05-25 00:35:22 +0200

    vercel

--- api/main.go
+++ api/main.go
@@ -5,8 +5,6 @@ import (
 	"net/http"
 	"time"
 
-	"main/handlers"
-
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
@@ -21,20 +19,20 @@ func Main() {
 		ReadTimeout:  1 * time.Second,
 	}
 
-	r.HandleFunc("/main-page", handlers.SendMainPage)
-	r.HandleFunc("/profile", handlers.SendProfilePage)
-	r.HandleFunc("/tag/{tag}", handlers.SendTagPage)
-	r.HandleFunc("/info", handlers.SendInfoPage)
-	r.HandleFunc("/img", handlers.SendCatImg)
+	r.HandleFunc("/main-page", SendMainPage)
+	r.HandleFunc("/profile", SendProfilePage)
+	r.HandleFunc("/tag/{tag}", SendTagPage)
+	r.HandleFunc("/info", SendInfoPage)
+	r.HandleFunc("/img", SendCatImg)
 
-	r.HandleFunc("/post/{id}", handlers.Post)
-	r.HandleFunc("/login", handlers.Login)
-	r.HandleFunc("/register", handlers.Register)
-	r.HandleFunc("/tags", handlers.GetTags)
-	r.HandleFunc("/user", handlers.UserRouter)
-	r.HandleFunc("/comment", handlers.CommentRouter)
-	r.HandleFunc("/debug/page", handlers.SendDebug)
-	r.HandleFunc("/debug/contents", handlers.Debug)
+	r.HandleFunc("/post/{id}", Post)
+	r.HandleFunc("/login", Login)
+	r.HandleFunc("/register", Register)
+	r.HandleFunc("/tags", GetTags)
+	r.HandleFunc("/user", UserRouter)
+	r.HandleFunc("/comment", CommentRouter)
+	r.HandleFunc("/debug/page", SendDebug)
+	r.HandleFunc("/debug/contents", Debug)
 	log.Fatal(srv.ListenAndServe())
 
 }
